91c2d1eb9eaca15bad96ef34f4183daf686cc509
Yonatan Melka <ybmelka2@gmail.com>
2020-01-30 00:14:54 -0600

    Create database models

---
+++ models/courses.js
@@ -0,0 +1,52 @@
+'use strict';
+const Sequelize = require('sequelize');
+
+module.exports = sequelize => {
+  class Course extends Sequelize.Model {}
+  Course.init(
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: '"title" is required',
+          },
+        },
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: '"description" is required',
+          },
+        },
+      },
+      estimatedTime: {
+        type: Sequelize.TEXT,
+      },
+      materialsNeeded: {
+        type: Sequelize.TEXT,
+      },
+    },
+    { sequelize }
+  );
+
+  // A 'course' can be associated with only one 'user'
+  Course.associate = model => {
+    Course.belongsTo(model.User, {
+      foreignKey: {
+        fieldName: 'userID',
+        allowNull: false,
+      },
+    });
+  };
+
+  return Course;
+};
---
+++ models/users.js
@@ -0,0 +1,64 @@
+'use strict';
+const Sequelize = require('sequelize');
+
+module.exports = sequelize => {
+  class User extends Sequelize.Model {}
+  User.init(
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: '"firstName" is required',
+          },
+        },
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: '"lastName" is required',
+          },
+        },
+      },
+      emailAddress: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: '"emailAddress" is required',
+          },
+        },
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: '"password" is required',
+          },
+        },
+      },
+    },
+    { sequelize }
+  );
+
+  // A 'user' can be associated with many 'courses'.
+  User.associate = model => {
+    User.hasMany(model.Course, {
+      foreignKey: {
+        fieldName: 'userId',
+        allowNull: false,
+      },
+    });
+  };
+
+  return User;
+};
